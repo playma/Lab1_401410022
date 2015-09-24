47995536da36c22326a46c7bae9e913791b1e9be
playma <scott820914@gmail.com>
2015-09-24 10:39:57 +0800

    finish

---
+++ part2.R
@@ -0,0 +1,10 @@
+sum(1:100*1:100)
+
+?iris
+?seq
+?mean
+?stats
+?swirl
+
+seq(1, 9, by = 2)
+
---
+++ part3.R
@@ -0,0 +1,49 @@
+#Wingcrd
+Wingcrd <- c(59,55,53.5,55,52.5,57.5,53,55)
+
+Wingcrd_mean <- mean(Wingcrd)
+Wingcrd_median <- median(Wingcrd)
+Wingcrd_min <- min(Wingcrd)
+Wingcrd_max <- max(Wingcrd)
+
+print(Wingcrd_mean)
+print(Wingcrd_median)
+print(Wingcrd_min)
+print(Wingcrd_max)
+
+#Tarsus
+Tarsus <-  c(22.3,19.7,20.8,20.3,20.8,21.5,20.6,21.5)
+Tarsus_mean <- mean(Tarsus)
+Tarsus_median <- median(Tarsus)
+Tarsus_min <- min(Tarsus)
+Tarsus_max <- max(Tarsus)
+
+print(Tarsus_mean)
+print(Tarsus_median)
+print(Tarsus_min)
+print(Tarsus_max)
+
+#Head
+Head <-  c(31.5,30.4,30.6,30.3,30.3,30.8,32.5,NA)
+
+Head_mean <- mean(Head)
+Head_median <- median(Head)
+Head_min <- min(Head)
+Head_max <- max(Head)
+
+print(Head_mean)
+print(Head_median)
+print(Head_min)
+print(Head_max)
+
+#Wt
+Wt <-  c(9.5,13.8,14.8,15.2,15.5,15.6,15.6,15.7)
+Wt_mean <- mean(Wt)
+Wt_median <- median(Wt)
+Wt_min <- min(Wt)
+Wt_max <- max(Wt)
+
+print(Wt_mean)
+print(Wt_median)
+print(Wt_min)
+print(Wt_max)
